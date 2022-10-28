475df8c644cc7ee0c6d5eba6e52c99f7bd11aa3b
tq-tuan <116077161+tq-tuan@users.noreply.github.com>
2022-10-28 14:36:16 +0700

    Update sui.sh

--- sui.sh
+++ sui.sh
@@ -23,7 +23,7 @@ sudo apt-get install jq -y
 
 # download sui binaries
 version=$(wget -qO- https://api.github.com/repos/MystenLabs/sui/releases/latest | jq -r ".tag_name")
-wget -qO- "https://github.com/MystenLabs/sui/releases/download/devnet-0.13.0/sui-linux-x86_64" | sudo tar -C /usr/local/bin/ -xzf -
+wget "https://github.com/MystenLabs/sui/releases/download/devnet-0.13.0/sui-linux-x86_64" | sudo tar -C /usr/local/bin/ -xzf -
 
 # download and update configs
 mkdir -p $HOME/.sui
